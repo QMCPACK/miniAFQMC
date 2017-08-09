69bab1affaee7b4c14a283b9f8698840fad52838
correaa <correaa@llnl.gov>
2017-08-08 17:55:59 -0700

    implemented multi_array gemv

--- src/Numerics/ma_blas.hpp
+++ src/Numerics/ma_blas.hpp
@@ -6,12 +6,52 @@
 
 #include "Numerics/OhmmsBlas.h"
 #include<utility> //std::enable_if
+#include<cassert>
 
-template<class T, class MultiArray, typename = typename std::enable_if<std::decay<MultiArray>::type::dimensionality == 1>::type >
-MultiArray& Scal(T a, MultiArray&& x){
+template<class T, class MultiArray1D, typename = typename std::enable_if<std::decay<MultiArray1D>::type::dimensionality == 1>::type >
+MultiArray1D& scal(T a, MultiArray1D&& x){
 	BLAS::scal(x.size(), a, x.origin(), x.strides()[0]);
 	return x;
 }
+
+template<class T, class MultiArray1D>
+auto operator*=(MultiArray1D&& x, T a) -> decltype(scal(a, std::forward<MultiArray1D>(x))){
+	return scal(a, std::forward<MultiArray1D>(x));
+}
+
+template<class T, class MultiArray1DA, class MultiArray1DB, 
+	typename = typename std::enable_if<std::decay<MultiArray1DA>::type::dimensionality == 1 and std::decay<MultiArray1DB>::type::dimensionality == 1>::type
+>
+MultiArray1DB& axpy(T x, MultiArray1DA const& a, MultiArray1DB&& b){
+	assert( a.shape()[0] == b.shape()[0] );
+	BLAS::axpy(a.shape()[0], x, a.origin(), a.strides()[0], b.origin(), b.strides()[0]);
+	return b;
+}
+
+
+  inline static void gemv(char trans_in, int n, int m, float alpha,
+                          const float *restrict amat, int lda, const float *x,
+                          int incx, float beta, float *y, int incy)
+  {
+    sgemv(trans_in, n, m, alpha, amat, lda, x, incx, beta, y, incy);
+  }
+
+
+template<char IN, class T, class MultiArray2DA, class MultiArray1DX, class MultiArray1DY>
+MultiArray1DY& gemv(T alpha, MultiArray2DA const& A, MultiArray1DX const& x, T beta, MultiArray1DY&& y){
+	assert(x.shape()[0] == y.shape()[0]);
+	int n = x.shape()[0];
+	int m = A.shape()[1];
+	assert(n == m);
+	BLAS::gemv(IN, n, m, alpha, A.origin(), A.strides()[0], x.origin(), x.strides()[0], beta, y.origin(), y.strides()[0]);
+	return y;
+} //y := alpha*A*x + beta*y,
+
+template<char IN = 'N', class MultiArray2DA, class MultiArray1DX, class MultiArray1DY>
+MultiArray1DY& gemv(MultiArray2DA const& A, MultiArray1DX const& x, MultiArray1DY&& y){
+	return gemv<IN>(1., A, x, 0., y);
+} //y := alpha*A*x
+
 
 #ifdef _TEST_MA_BLAS
 
@@ -25,7 +65,7 @@ int main(){
 	std::vector<double> v = {1.,2.,3.};
 	{
 		boost::multi_array_ref<double, 1> V(v.data(), boost::extents[v.size()]);
-		Scal(2., V);
+		scal(2., V);
 		{
 			std::vector<double> v2 = {2.,4.,6.};
 			boost::multi_array_ref<double, 1> V2(v2.data(), boost::extents[v2.size()]);
@@ -40,7 +80,7 @@ int main(){
 	};
 	boost::multi_array_ref<double, 2> M(m.data(), boost::extents[3][3]);
 	assert( M.num_elements() == m.size());
-	Scal(2., M[2]);
+	scal(2., M[2]);
 	{
 		std::vector<double> m2 = {
 			1.,2.,3.,
@@ -53,7 +93,7 @@ int main(){
 	typedef boost::multi_array_types::index_range range_t;
 	boost::multi_array_types::index_gen indices;
 
-	Scal(2., M[ indices[range_t(0,3)][2] ]);
+	scal(2., M[ indices[range_t(0,3)][2] ]);
 	{
 		std::vector<double> m2 = {
 			1.,2.,6.,
@@ -63,7 +103,7 @@ int main(){
 		boost::multi_array_ref<double, 2> M2(m2.data(), boost::extents[3][3]);
 		assert( M == M2 );
 	}
-	Scal(2., M[ indices[range_t(0,2)][1] ]);
+	scal(2., M[ indices[range_t(0,2)][1] ]);
 	{
 		std::vector<double> m2 = {
 			1.,4.,6.,
@@ -73,7 +113,31 @@ int main(){
 		boost::multi_array_ref<double, 2> M2(m2.data(), boost::extents[3][3]);
 		assert( M == M2 );
 	}
-
+	axpy(2., M[1], M[0]); // M[0] += a*M[1]
+	{
+		std::vector<double> m2 = {
+			9.,24.,30.,
+			4.,10.,12.,
+			14.,16.,36.
+		};
+		boost::multi_array_ref<double, 2> M2(m2.data(), boost::extents[3][3]);
+		assert( M == M2 );
+	}
+	{
+		std::vector<double> x = {1.,2.,3.};
+		boost::multi_array_ref<double, 1> X(x.data(), boost::extents[x.size()]);
+		std::vector<double> y = {4.,5.,6.};
+		boost::multi_array_ref<double, 1> Y(y.data(), boost::extents[y.size()]);
+		gemv<'N'>(1., M, X, 0., Y); // y := M^T x
+		for(int i = 0; i != 3; ++i) cout << Y[i] << " ";
+		cout << '\n';
+		gemv<'T'>(1., M, X, 0., Y); // y := M x
+		for(int i = 0; i != 3; ++i) cout << Y[i] << " ";
+		cout << '\n';
+		gemv<'T'>(2., M, X, 3.3, Y); // y := 2 M x + 3.3 y
+		for(int i = 0; i != 3; ++i) cout << Y[i] << " ";
+		cout << '\n';
+	}
 }
 
 #endif
