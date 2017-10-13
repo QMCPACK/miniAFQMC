16ab7ea9c67f3ceeb73aeca741fbdbe48ccd09c4
Xavier Andrade <xavier@tddft.org>
2017-10-12 21:40:59 -0700

    Error check to cusparse calls.

--- src/Numerics/cusparse.hpp
+++ src/Numerics/cusparse.hpp
@@ -15,87 +15,116 @@ namespace cusparse {
   void end(){
     cusparseDestroy(handle);
   }
+
+  void check_status(cusparseStatus_t status){
+    switch(status) {
+    case CUSPARSE_STATUS_SUCCESS :
+      return;
+    case CUSPARSE_STATUS_NOT_INITIALIZED :
+      std::cout << "cusparse: the library was not initialized."  << std::endl;
+      break;
+    case CUSPARSE_STATUS_INVALID_VALUE :
+      std::cout << "cusparse: invalid parameters were passed (m,n <0)."  << std::endl;
+      break;
+    case CUSPARSE_STATUS_ARCH_MISMATCH :
+      std::cout << "cusparse: the device does not support double precision."  << std::endl;
+      break;
+    case CUSPARSE_STATUS_EXECUTION_FAILED :
+      std::cout << "cusparse: the function failed to launch on the GPU."  << std::endl;
+      break;
+    case CUSPARSE_STATUS_INTERNAL_ERROR :
+      std::cout << "cusparse: an internal operation failed."  << std::endl;
+      break;
+    case CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED :
+      std::cout << "cusparse: the matrix type is not supported."  << std::endl;
+      break;
+    default :
+      std::cout << "cusparse: Unknown error."  << std::endl;
+      break;
+    }
+    exit(1);
+  }
   
-  cusparseStatus_t csrmm2(cusparseOperation_t      transA,    
-			  cusparseOperation_t      transB,    
-			  int                      m,         
-			  int                      n,         
-			  int                      k,         
-			  int                      nnz,       
-			  const float              *alpha,    
-			  const cusparseMatDescr_t descrA, 
-			  const float              *csrValA, 
-			  const int                *csrRowPtrA, 
-			  const int                *csrColIndA,
-			  const float              *B,
-			  int                      ldb,
-			  const float              *beta,
-			  float                    *C,
-			  int                      ldc){
+  void csrmm2(cusparseOperation_t      transA,    
+	      cusparseOperation_t      transB,    
+	      int                      m,         
+	      int                      n,         
+	      int                      k,         
+	      int                      nnz,       
+	      const float              *alpha,    
+	      const cusparseMatDescr_t descrA, 
+	      const float              *csrValA, 
+	      const int                *csrRowPtrA, 
+	      const int                *csrColIndA,
+	      const float              *B,
+	      int                      ldb,
+	      const float              *beta,
+	      float                    *C,
+	      int                      ldc){
     
-    return cusparseScsrmm2(handle, transA, transB, m, n, k, nnz, alpha, descrA, csrValA, csrRowPtrA, csrColIndA, B, ldb, beta, C, ldc);
+    check_status(cusparseScsrmm2(handle, transA, transB, m, n, k, nnz, alpha, descrA, csrValA, csrRowPtrA, csrColIndA, B, ldb, beta, C, ldc));
   }
   
   
   
-  cusparseStatus_t csrmm2(cusparseOperation_t      transA, 
-			  cusparseOperation_t      transB,
-			  int                      m,
-			  int                      n,
-			  int                      k,
-			  int                      nnz, 
-			  const double             *alpha, 
-			  const cusparseMatDescr_t descrA, 
-			  const double             *csrValA, 
-			  const int                *csrRowPtrA,
-			  const int                *csrColIndA,
-			  const double             *B,
-			  int                      ldb,
-			  const double             *beta,
-			  double                   *C,
-			  int                      ldc){
-  
-    return cusparseDcsrmm2(handle, transA, transB, m, n, k, nnz, alpha, descrA, csrValA, csrRowPtrA, csrColIndA, B, ldb, beta, C, ldc);
+  void csrmm2(cusparseOperation_t      transA, 
+	      cusparseOperation_t      transB,
+	      int                      m,
+	      int                      n,
+	      int                      k,
+	      int                      nnz, 
+	      const double             *alpha, 
+	      const cusparseMatDescr_t descrA, 
+	      const double             *csrValA, 
+	      const int                *csrRowPtrA,
+	      const int                *csrColIndA,
+	      const double             *B,
+	      int                      ldb,
+	      const double             *beta,
+	      double                   *C,
+	      int                      ldc){
+    
+    check_status(cusparseDcsrmm2(handle, transA, transB, m, n, k, nnz, alpha, descrA, csrValA, csrRowPtrA, csrColIndA, B, ldb, beta, C, ldc));
   }
 
-  cusparseStatus_t csrmm2(cusparseOperation_t       transA, 
-			  cusparseOperation_t       transB,
-			  int                       m,
-			  int                       n,
-			  int                       k,
-			  int                       nnz, 
-			  const std::complex<float> *alpha, 
-			  const cusparseMatDescr_t  descrA, 
-			  const std::complex<float> *csrValA, 
-			  const int                 *csrRowPtrA,
-			  const int                 *csrColIndA,
-			  const std::complex<float> *B,
-			  int                       ldb,
-			  const std::complex<float> *beta,
-			  std::complex<float>       *C,
-			  int                       ldc){
-
-    return cusparseCcsrmm2(handle, transA, transB, m, n, k, nnz, (const cuComplex*) alpha, descrA, (const cuComplex*) csrValA, csrRowPtrA, csrColIndA, (const cuComplex*) B, ldb, (const cuComplex*) beta, (cuComplex*) C, ldc);
+  void csrmm2(cusparseOperation_t       transA, 
+	      cusparseOperation_t       transB,
+	      int                       m,
+	      int                       n,
+	      int                       k,
+	      int                       nnz, 
+	      const std::complex<float> *alpha, 
+	      const cusparseMatDescr_t  descrA, 
+	      const std::complex<float> *csrValA, 
+	      const int                 *csrRowPtrA,
+	      const int                 *csrColIndA,
+	      const std::complex<float> *B,
+	      int                       ldb,
+	      const std::complex<float> *beta,
+	      std::complex<float>       *C,
+	      int                       ldc){
+    
+    check_status(cusparseCcsrmm2(handle, transA, transB, m, n, k, nnz, (const cuComplex*) alpha, descrA, (const cuComplex*) csrValA, csrRowPtrA, csrColIndA, (const cuComplex*) B, ldb, (const cuComplex*) beta, (cuComplex*) C, ldc));
   }
   
-  cusparseStatus_t csrmm2(cusparseOperation_t        transA, 
-			  cusparseOperation_t        transB,
-			  int                        m,
-			  int                        n,
-			  int                        k,
-			  int                        nnz, 
-			  const std::complex<double> *alpha, 
-			  const cusparseMatDescr_t   descrA, 
-			  const std::complex<double> *csrValA, 
-			  const int                  *csrRowPtrA,
-			  const int                  *csrColIndA,
-			  const std::complex<double> *B,
-			  int                        ldb,
-			  const std::complex<double> *beta,
-			  std::complex<double>       *C,
-			  int                        ldc){
+  void csrmm2(cusparseOperation_t        transA, 
+	      cusparseOperation_t        transB,
+	      int                        m,
+	      int                        n,
+	      int                        k,
+	      int                        nnz, 
+	      const std::complex<double> *alpha, 
+	      const cusparseMatDescr_t   descrA, 
+	      const std::complex<double> *csrValA, 
+	      const int                  *csrRowPtrA,
+	      const int                  *csrColIndA,
+	      const std::complex<double> *B,
+	      int                        ldb,
+	      const std::complex<double> *beta,
+	      std::complex<double>       *C,
+	      int                        ldc){
     
-    return cusparseZcsrmm2(handle, transA, transB, m, n, k, nnz, (const cuDoubleComplex*) alpha, descrA, (const cuDoubleComplex*) csrValA, csrRowPtrA, csrColIndA, (const cuDoubleComplex*) B, ldb, (const cuDoubleComplex*) beta, (cuDoubleComplex*) C, ldc);
+    check_status(cusparseZcsrmm2(handle, transA, transB, m, n, k, nnz, (const cuDoubleComplex*) alpha, descrA, (const cuDoubleComplex*) csrValA, csrRowPtrA, csrColIndA, (const cuDoubleComplex*) B, ldb, (const cuDoubleComplex*) beta, (cuDoubleComplex*) C, ldc));
   }
 
   template<typename op_tag_type>
